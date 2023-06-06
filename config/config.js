8e853a62afce62afe029ee73be2242a6e02631c8
Emmanuel <Emmanuelmuya@gmail.com>
2023-06-06 17:28:45 +0200

    Updates

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const dbDetails = {
   username: "admin",
-  password: "#Manny3000",
+  password: "Emman0099012",
   database: "myapp",
   host: "database-1.cwixvtlqqegj.eu-north-1.rds.amazonaws.com",
   dialect: "mysql",
